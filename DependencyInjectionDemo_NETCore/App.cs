0215a2d640aa4a8bb80a0cb24748ca17fd6363e9
RyanLeeYi <ian4567x@gmail.com>
2023-10-03 13:16:24 +0800

    Initial commit

---
+++ DependencyInjectionDemo_NETCore/App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo_NETCore
+{
+    public class App
+    {
+        private readonly IService _service;
+
+        public App(IService service)
+        {
+            _service = service;
+        }
+
+        public void Run()
+        {
+            _service.SayHello();
+        }
+    }
+}
---
+++ DependencyInjectionDemo_NETCore/IService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo_NETCore
+{
+    public interface IService
+    {
+        void SayHello();
+    }
+    public class ChtService : IService
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("哈囉世界！");
+        }
+    }
+
+    public class EngService : IService
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
